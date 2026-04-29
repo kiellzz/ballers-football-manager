86ca60bb4c4d415970abbb84fabb6b10e090ee14
Ezequiel David Borges <ezequiellsouzaa13@gmail.com>
2026-04-29 01:38:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Ballers_CRUD/",
+  base: "/ballers-football-manager/",
   plugins: [react()],
 });
